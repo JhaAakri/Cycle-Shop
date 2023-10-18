e8ab3486847eb7035a1cbffbc3a3a54cfbe1fa55
sujalsahu620 <sahusujal620@gmail.com>
2023-10-18 20:51:28 +0530

    js and header remains

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
-var data={
-    "large_img": "https://i.ibb.co/bLcS2ZZ/cycle.jpg",
-    "img2": "https://m.media-amazon.com/images/I/91yzwj5bjpL._SY355_.jpg",
-    "img3": "https://m.media-amazon.com/images/I/81YJaUtm2fL._SY355_.jpg",
-    "img4": "https://m.media-amazon.com/images/I/811GabfBunL._SY355_.jpg",
-    "img5": "https://m.media-amazon.com/images/I/91dxKrTuQAL._SY355_.jpg",
-    "title": "Lifelong Conqueror Freeride Shimano Gear Cycle",
-    "frame_colors": [
-      "red"
-    ],
-    "front_wheel": "Traversal SL 29",
-    "rear_wheel": "TRAVERSE SL 29",
-    "Front_tire": "back",
-    "inner_tubes": "STANDARD, PRESTA VALVE",
-    "front_Wheel": "BUTCHER GRID TRAIL T7 29 * 2.3",
-    "description": "This Shimano 21 speed geared cycle comes with Disc Brakes and suspension for effortless Braking system built for your ease of use and maintenance, and excellent braking power. Specially designed to provide proper braking control on all terrains. The cycle has a sturdy and reliable steel frame that can brave rough riding conditions and terrains. The top-notch construction quality ensures that the bike has a long lifespan",
-    "rating": 4,
-    "size": "27.5T",
-    "year": 4520,
-    "price": 11520,
-    "category": "active",
-    "img1": "",
-    "Front Tire": "",
-    "frame_model": "",
-    "id": "bic1",
-    "manufacturer_name": "",
-    "serial": "",
-    "thumb": "",
-    "url": "",
-    "status": "",
-    "date_stolen": "",
-    "is_stock_img": "",
-    "location_found": "",
-    "external_id": "",
-    "registry_name": "",
-    "registry_url": "",
-    "stolen": "",
-    "stolen_coordinates": "",
-    "stolen_location": "",
-    "exp": ""
-  }
-  
+var url="https://bicycle-shop-json-server.cyclic.app/bikes?id="
+
+// main images
+let large_img=document.getElementById("large_img")
+large_img.setAttribute("img",)
+
+let img2=document.getElementById("img2")
+img2.setAttribute("img",)
+
+let img3=document.getElementById("img3")
+img3.setAttribute("img",)
+
+let img4=document.getElementById("img4")
+img4.setAttribute("img",)
+
+let img5=document.getElementById("img5")
+img5.setAttribute("img",)
+
