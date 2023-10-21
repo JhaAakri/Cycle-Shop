53daa622677077e7b067168dce0752fb720a779f
sujalsahu620 <sahusujal620@gmail.com>
2023-10-21 17:35:22 +0530

    done

---
+++ product_detail.js
@@ -0,0 +1,184 @@
+
+
+var url="https://bicycle-shop-json-server.cyclic.app/bikes?id=bic1"
+
+async function fetchData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+const citiesData = [];
+const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+// Fetch data only once
+async function getData() {
+    const data = await fetchData(url);
+    citiesData.push(...data);
+}
+
+getData();
+
+
+console.log(citiesData);
+
+
+// var id;
+// var newdata=[]
+// fetchData()
+// function fetchData() {
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+//             newdata = data;
+//            // updatedom(newdata);
+//         })
+//         .catch(error => console.error(error));
+// }
+
+// console.log(newdata)
+
+// // buttons 
+// // add to cart button
+// let addtocart=document.getElementById("addtocartbtn")
+// addtocart.addEventListener("click",()=>{
+//     localStorage.setItem("id",id)
+//     //set the product id to localstorage
+// })
+// //quick order button
+// let quickorderbtn=document.getElementById("quickorderbtn")
+// quickorderbtn.addEventListener("click",()=>{
+//     //link to address page
+// })
+// // console.log(cycle)
+
+// function updatedom(data){
+//     id=data.id
+//    // console.log(id)
+//     //main image
+//     newdata.forEach(data => {
+//     let large_img=document.getElementById("large_img")
+//     large_img.setAttribute("src",data.large_img)
+    
+//     //sub image
+//     let img2=document.getElementById("img2")
+//     img2.setAttribute("src",data.img2)
+    
+//     let img3=document.getElementById("img3")
+//     img3.setAttribute("src",data.img3)
+    
+//     let img4=document.getElementById("img4")
+//     img4.setAttribute("src",data.img4)
+    
+//     let img5=document.getElementById("img5")
+//     img5.setAttribute("src",data.img5)
+    
+//     //title
+//     let title=document.getElementById("title")
+//     title.innerText=data.title
+    
+//     //S/N
+//     let year=document.getElementById("year")
+//     year.innerText=`S/N:- ${data.year}`
+    
+//     //price
+//     let price=document.getElementById("price")
+//     price.innerText=`$ ${data.year}.00`
+    
+//     //characterstics
+//     let front_wheel=document.getElementById("front_wheel")
+//     front_wheel.innerText=data.front_wheel
+    
+//     let rear_wheel=document.getElementById("rear_wheel")
+//     rear_wheel.innerText=data.rear_wheel
+    
+//     let front_tire=document.getElementById("front_tire")
+//     front_tire.innerText=data.front_Wheel
+    
+//     let inner_tubes=document.getElementById("inner_tubes")
+//     inner_tubes.innerText=data.inner_tubes
+    
+//     let inner_tire=document.getElementById("inner_tire")
+//     inner_tire.innerText=data.front_Wheel
+    
+//     //description
+//     let description=document.getElementById("description_text")
+//     description.innerText=data.description
+    
+//     //category
+//     let category=document.getElementById("category")
+//     category.innerText=`Category:- ${data.category}`
+
+//     })
+
+// }
+
+    // async function getData(url){
+    //     try {
+    //         let res=await fetch(url);
+    //         let data=await res.json();
+    //         updatedom(data[0])
+    //         id=JSON.stringify(data[0].id)
+    //         function updatedom(data){
+    //             id=data.id
+    //             console.log(id)
+    //             //main image
+    //             let large_img=document.getElementById("large_img")
+    //             large_img.setAttribute("src",data.large_img)
+    
+    //             //sub image
+    //             let img2=document.getElementById("img2")
+    //             img2.setAttribute("src",data.img2)
+            
+    //             let img3=document.getElementById("img3")
+    //             img3.setAttribute("src",data.img3)
+            
+    //             let img4=document.getElementById("img4")
+    //             img4.setAttribute("src",data.img4)
+            
+    //             let img5=document.getElementById("img5")
+    //             img5.setAttribute("src",data.img5)
+            
+    //             //title
+    //             let title=document.getElementById("title")
+    //             title.innerText=data.title
+            
+    //             //S/N
+    //             let year=document.getElementById("year")
+    //             year.innerText=`S/N:- ${data.year}`
+    
+    //             //price
+    //             let price=document.getElementById("price")
+    //             price.innerText=`$ ${data.year}.00`
+    
+    //             //characterstics
+    //             let front_wheel=document.getElementById("front_wheel")
+    //             front_wheel.innerText=data.front_wheel
+    
+    //             let rear_wheel=document.getElementById("rear_wheel")
+    //             rear_wheel.innerText=data.rear_wheel
+    
+    //             let front_tire=document.getElementById("front_tire")
+    //             front_tire.innerText=data.front_Wheel
+    
+    //             let inner_tubes=document.getElementById("inner_tubes")
+    //             inner_tubes.innerText=data.inner_tubes
+    
+    //             let inner_tire=document.getElementById("inner_tire")
+    //             inner_tire.innerText=data.front_Wheel
+    
+    //             //description
+    //             let description=document.getElementById("description_text")
+    //             description.innerText=data.description
+    
+    //             //category
+    //             let category=document.getElementById("category")
+    //             category.innerText=`Category:- ${data.category}`
+    //         }
+            
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
+    // getData(url);
+    // console.log(id)
