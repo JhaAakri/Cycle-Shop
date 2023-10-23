63185df28eef81f3405e22f0c6b531bc9355e7b9
sujalsahu620 <sahusujal620@gmail.com>
2023-10-23 19:39:33 +0530

    done

--- product_detail.js
+++ product_detail.js
@@ -1,34 +1,70 @@
-var id=localStorage.getitem("item-id")
-
-var baseurl="https://bicycle-shop-json-server.cyclic.app/bikes?id="
-var url=baseurl+id
+var data=localStorage.getitem("prodct-detail-data")
 
 
+// var data=[{
+//     "large_img": "https://i.ibb.co/bLcS2ZZ/cycle.jpg",
+//     "img2": "https://m.media-amazon.com/images/I/91yzwj5bjpL._SY355_.jpg",
+//     "img3": "https://m.media-amazon.com/images/I/81YJaUtm2fL._SY355_.jpg",
+//     "img4": "https://m.media-amazon.com/images/I/811GabfBunL._SY355_.jpg",
+//     "img5": "https://m.media-amazon.com/images/I/91dxKrTuQAL._SY355_.jpg",
+//     "title": "Lifelong Conqueror Freeride Shimano Gear Cycle",
+//     "frame_colors": [
+//       "red"
+//     ],
+//     "front_wheel": "Traversal SL 29",
+//     "rear_wheel": "TRAVERSE SL 29",
+//     "Front_tire": "back",
+//     "inner_tubes": "STANDARD, PRESTA VALVE",
+//     "front_Wheel": "BUTCHER GRID TRAIL T7 29 * 2.3",
+//     "description": "This Shimano 21 speed geared cycle comes with Disc Brakes and suspension for effortless Braking system built for your ease of use and maintenance, and excellent braking power. Specially designed to provide proper braking control on all terrains. The cycle has a sturdy and reliable steel frame that can brave rough riding conditions and terrains. The top-notch construction quality ensures that the bike has a long lifespan",
+//     "rating": 4,
+//     "size": "27.5T",
+//     "year": 4520,
+//     "price": 11520,
+//     "category": "active",
+//     "img1": "",
+//     "Front Tire": "",
+//     "frame_model": "",
+//     "id": "bic1",
+//     "manufacturer_name": "",
+//     "serial": "",
+//     "thumb": "",
+//     "url": "",
+//     "status": "",
+//     "date_stolen": "",
+//     "is_stock_img": "",
+//     "location_found": "",
+//     "external_id": "",
+//     "registry_name": "",
+//     "registry_url": "",
+//     "stolen": "",
+//     "stolen_coordinates": "",
+//     "stolen_location": "",
+//     "exp": ""
+//   }]
 // buttons 
 // add to cart button
 let addtocart=document.getElementById("addtocartbtn")
 addtocart.addEventListener("click",()=>{
-    localStorage.setItem("id",)
-    //set the product id to localstorage
+    localStorage.setItem("product-cart-data",[...data])
+    //window.location.
+    //link to cart page
 })
 //quick order button
 let quickorderbtn=document.getElementById("quickorderbtn")
 quickorderbtn.addEventListener("click",()=>{
+    localStorage.setItem("product-checkout-data",[...data])
+    //window.location.
     //link to address page
 })
 // console.log(cycle)
 
-
+updatedom(data[0])
 
   
 //slider ends
-getData(url);
-    async function getData(url){
-        try {
-            let res=await fetch(url);
-            let data=await res.json();
-            updatedom(data[0])
-            function updatedom(data){
+
+function updatedom(data){
                // main image
                 let large_img=document.getElementById("large_img")
                 large_img.setAttribute("src",data.large_img)
@@ -82,10 +118,5 @@ getData(url);
                 let category=document.getElementById("category")
                 category.innerText=`Category:- ${data.category}`
             }
-            
-        } catch (error) {
-            console.log(error)
-        }
-    }
 
     // console.log(id)
